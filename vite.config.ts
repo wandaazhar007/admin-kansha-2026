7209c4dfe19b5f2da9cdaa2017640c5b0d75ce14
wanda azhar <wandaazhar@gmail.com>
2026-03-12 12:23:08 -0600

    Refactor Code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-router")) {
+              return "router";
+            }
+
+            if (id.includes("firebase")) {
+              return "firebase";
+            }
+
+            if (id.includes("@fortawesome")) {
+              return "fontawesome";
+            }
+
+            if (id.includes("react") || id.includes("react-dom")) {
+              return "react-vendor";
+            }
+
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
